1a58ab40428f7d26aef3c422d0e7e8a08a052c42
Rory <rory@fancysquares.com>
2025-03-05 10:06:11 -0800

    production build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks'), 'version' => 'd0acc33d03817bdfbe22');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks'), 'version' => 'cea0a6398726adb50a52');
